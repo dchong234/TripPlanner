9fe487238ac2ab635de8e7b24c6458ad94f54439
khandrew1 <akhad007@ucr.edu>
2023-05-16 21:56:48 -0700

    popualted Activity

--- include/Activity.h
+++ include/Activity.h
@@ -1,4 +1,15 @@
 #ifndef ACTIVITY_H
 #define ACTIVITY_H
+#include <string>
+
+class Activity {
+    private:
+        string name;
+        string time;
+        double price;
+    public:
+        Activity();
+        string getActivity();
+};
 
 #endif
