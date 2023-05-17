4f20841a587f56066862b4f1beef7565a1388063
khandrew1 <akhad007@ucr.edu>
2023-05-16 22:01:15 -0700

    populated Hotel.h

--- include/Hotel.h
+++ include/Hotel.h
@@ -1,4 +1,16 @@
 #ifndef HOTEL_H
 #define HOTEL_H
+#include <string>
+
+class Hotel {
+    private:
+        string name;
+        string location;
+        double price;
+        int rating;
+    public:
+        Hotel();
+        string getHotel();
+}
 
 #endif
