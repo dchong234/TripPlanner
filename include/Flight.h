67f345d0172e20605b860136a5297fa02a6fb3f5
khandrew1 <akhad007@ucr.edu>
2023-05-16 21:58:20 -0700

    populated Flight

--- include/Flight.h
+++ include/Flight.h
@@ -1,4 +1,15 @@
 #ifndef FLIGHT_H
 #define FLIGHT_H
+#include <string>
+
+class Flight {
+    private:
+        string name;
+        string time;
+        double price;
+    public:
+        Flight();
+        string getFlight();
+}
 
 #endif
