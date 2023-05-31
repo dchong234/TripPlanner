093d189e0c3ebb89df1cca4fbdda61e475a2a316
colinthekkinedath <colinthekkinedath@gmail.com>
2023-05-30 23:14:17 -0700

    fixed Hotel class

--- include/Hotel.h
+++ include/Hotel.h
@@ -9,10 +9,10 @@ class Hotel {
         double price;
         int rating;
     public:
-        Hotel(string, string, double, int);
+        Hotel(std::string, std::string, double, int);
         std::string getName();
         std::string getLocation();
-        doubble getPrice();
+        double getPrice();
         int getRating();
         std::string getHotel();
 };
