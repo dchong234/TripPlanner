bf091f60170460bd6d96391d154be2546af333ea
khandrew1 <akhad007@ucr.edu>
2023-05-30 22:57:13 -0700

    refactored AccountHandler

--- include/AccountHandler.h
+++ include/AccountHandler.h
@@ -1,5 +1,8 @@
 #ifndef ACCOUNTHANDLER_H
 #define ACCOUNTHANDLER_H
+
+#include "User.h"
+
 #include <string>
 #include <vector>
 
@@ -7,6 +10,10 @@ class AccountHandler {
     private:
         std::string username;
         std::string password;
+
+        User user;
+        std::vector<User> userStorage;
+        
         std::vector<std::string> usernameStorage;
         std::vector<std::string> passwordStorage;
     public:
--- include/User.h
+++ include/User.h
@@ -1,17 +1,24 @@
 #ifndef USER_H
 #define USER_H
 
-#include "Schedule.h"
+// #include "Schedule.h"
 #include <string>
+#include <iostream>
 
 class User {
     private:
-        string name;
-        string username;
-        string password;
+        std::string username;
+        std::string password;
     public:
-        string getName();
-        string getUsername();
+        User() : username(""), password("") {}
+        User(std::string username, std::string password) : username(username), password(password) {}
+        std::string getUsername() { return username; }
+        std::string getPassword() { return password; }
+        friend std::ostream& operator<<(std::ostream& os, const User& user) {
+            os << "Username: " << user.username << std::endl;
+            os << "Password: " << user.password << std::endl;
+            return os;
+        }
 };
 
 #endif
