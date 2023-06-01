01c3377eca9a40511fe1dead6fd814a95a92844b
dchong234 <davidchong020304@gmail.com>
2023-05-31 23:01:04 -0700

    changed vectors to pointers

--- src/Schedule.cpp
+++ src/Schedule.cpp
@@ -9,26 +9,35 @@ void Schedule::getSchedule(){
   int time = 800;
 
   for(int i = 0; i < 10; i++){
-    for (auto x : this->trip->getFlights()) {
+    for (auto x : *(this->trip->getFlights())) {
       if (x->getDepartureTime() == time) {
         std::cout << time << " " << x->getAirline() << std::endl;
         time += 30;
+        if (((time % 100) / 10) > 5) {
+          time += 40;
+        }
         continue;
       }
     }
 
-    for (auto x : this->trip->getHotels()) {
+    for (auto x : *(this->trip->getHotels())) {
       if (x->getHotelTime() == time) {
         std::cout << time << " " << x->getName() << std::endl;
         time += 30;
+        if (((time % 100) / 10) > 5) {
+          time += 40;
+        }
         continue;
       }
     }
 
-    for (auto x : this->trip->getActivities()) {
+    for (auto x : *(this->trip->getActivities())) {
       if (x->getTime() == time) {
         std::cout << time << " " << x->getName() << std::endl;
         time += 30;
+        if (((time % 100) / 10) > 5) {
+          time += 40;
+        }
         continue;
       }
     }
@@ -40,6 +49,10 @@ void Schedule::getSchedule(){
       time += 40;
     }
   }
+}
+
+void Schedule::printStuff() {
+  std::cout << trip->getActivities()->at(0)->getName() << std::endl;
 }
 
 void Schedule::addActvity(Activity* activity){
--- src/Trip.cpp
+++ src/Trip.cpp
@@ -4,20 +4,23 @@
 
 Trip::Trip(std::string name){
     this->name = name;
+    this->flights = new std::vector<Flight*>();
+    this->hotels = new std::vector<Hotel*>();
+    this->activities = new std::vector<Activity*>();
 }
 
 std::string Trip::getTripName(){
     return this->name;
 }
 
-std::vector<Flight*> Trip::getFlights() {
+std::vector<Flight*> * Trip::getFlights() {
     return this->flights;
 };
 
-std::vector<Hotel*> Trip::getHotels() {
+std::vector<Hotel*> * Trip::getHotels() {
     return this->hotels;
 };
 
-std::vector<Activity*> Trip::getActivities() {
+std::vector<Activity*> * Trip::getActivities() {
     return this->activities;
 };
