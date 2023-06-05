5bb34e1b14bc216ccee14b84d3b921238abe9dcb
colinthekkinedath <colinthekkinedath@gmail.com>
2023-06-05 16:19:54 -0700

    added formatTime Function

--- include/TripItem.h
+++ include/TripItem.h
@@ -1,6 +1,8 @@
 #ifndef TRIPITEM_H
 #define TRIPITEM_H
 #include <string>
+#include <ctime>
+#include <iomanip>
 
 enum ItemType {ACTIVITY, FLIGHT, HOTEL};
 
@@ -13,6 +15,16 @@ class TripItem{
     ItemType getItemType() { return type; }
     std::string getID() { return id; }
     virtual std::string getItem() = 0;
+    std::string formatTime(int time) const {
+        int hours = time / 100;
+        int minutes = time % 100;
+        std::tm tm = {};
+        tm.tm_hour = hours;
+        tm.tm_min = minutes;
+        std::ostringstream oss;
+        oss << std::put_time(&tm, "%I:%M %p");
+        return oss.str();
+    }
 };
 
 
