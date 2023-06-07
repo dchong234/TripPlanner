538766c16d242f11467fbec2cde287d34a4f07a5
KeshavMannem <keshavmannem023@gmail.com>
2023-06-06 23:05:49 -0700

    added first google test

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,4 @@
+#include "gtest/gtest.h"
 #include "../include/MainMenu.h"
 #include "../include/AccountHandler.h"
 #include "../include/Trip.h"
@@ -5,11 +6,12 @@
 #include "../include/Schedule.h"
 #include "../include/DummyData.h"
 #include "../include/TripItem.h"
-#include "../include/Flight.h"
-#include <gtest/gtest.h>
+#include "../include/Activity.h"
 
-TEST(FlightTests, nameOfAirline) {
-    Flight flight("id", "Southwest", 800, 1200, "San Jose", "Tokyo", 115.25);
-    ASSERT_EQ(flight.getOrigin(), "San Jose");
+#include <string>
+
+TEST(FlightTests, originTest) {
+    Activity activity("F1", "kayaking", 800, 115.25);
+    ASSERT_DOUBLE_EQ(activity.getPrice(), 115.25);
 }
 
