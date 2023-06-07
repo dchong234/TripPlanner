014e76624c4ffa143f8c351f36ff5d05ac2e382f
KeshavMannem <keshavmannem023@gmail.com>
2023-06-06 20:32:09 -0700

    added CMake for src folder

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include "../include/MainMenu.h"
+#include "../include/AccountHandler.h"
+#include "../include/Trip.h"
+#include "../include/TripHandler.h"
+#include "../include/Schedule.h"
+#include "../include/DummyData.h"
+#include "../include/TripItem.h"
+#include "../include/Flight.h"
+#include "gtest/gtest.h"
+
+TEST(FlightTests, nameOfAirline) {
+    Flight flight("id", "Southwest", 800, 1200, "San Jose", "Tokyo", 115.25);
+    ASSERT_EQ(flight.getOrigin(), "San Jose");
+}
+
