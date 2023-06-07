35b5bf9eb62047fd00c8a08d1fcabbe1d64bae29
KeshavMannem <keshavmannem023@gmail.com>
2023-06-07 00:00:19 -0700

    added rating test for Hotel Suite

--- test/test.cpp
+++ test/test.cpp
@@ -58,3 +58,8 @@ TEST(HotelTests, priceTest) {
     Hotel hotel("H1", "La Quinta", "New York City", 150.25, 4, 1300);
     EXPECT_DOUBLE_EQ(hotel.getPrice(), 150.25);
 }
+
+TEST(HotelTests, ratingTest) {
+    Hotel hotel("H1", "La Quinta", "New York City", 150.25, 4, 1300);
+    ASSERT_EQ(hotel.getRating(), 4);
+}
